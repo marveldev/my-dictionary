7753a80650f4ead22e6067de3eb659612c7f0bad
marveldev <marvellousalika48@gmail.com>
2022-01-13 14:29:14 +0100

    add search book illustration svg

--- src/components/searchOutput/SearchOutput.jsx
+++ src/components/searchOutput/SearchOutput.jsx
@@ -1,7 +1,12 @@
+import { default as searchBookIllustration } from './searchBookIllustration.svg'
+
 const SearchOutput = () => {
   return (
     <div>
-      output container
+      <div className="search-book-illustration">
+        <img src={searchBookIllustration} alt="search book illustration"/>
+        <p>Find meanings to words you care about</p>
+      </div>
     </div>
   )
 }

cd4680e499cdde1103830b78f86e0003f1e60544
marveldev <marvellousalika48@gmail.com>
2022-01-13 21:05:45 +0100

    add fetch search data functionality

--- src/components/form/Form.jsx
+++ src/components/form/Form.jsx
@@ -1,26 +1,45 @@
-import { TextField } from '@mui/material'
+import debounce from 'lodash.debounce'
+import { useState } from 'react'
+import { MenuItem, TextField } from '@mui/material'
 
 const Form = () => {
-  const languages = ['English', 'India', 'China']
+  const languages = ['English(EN)', 'French(FR)', 'German(DE)', 'Spanish(ES)', 'Italian(IT)']
+  const [selectedLanguage, setSelectedLanguage] = useState('English(EN)')
+
+  const fetchSearchData = debounce(async value => {
+    if (value.trim().length >= 1) {
+      const languageShortCode = selectedLanguage.slice(-3, -1)
+      const url = `https://api.dictionaryapi.dev/api/v2/entries/${languageShortCode}/${value}`
+      const response = await fetch(url)
+      const data = await response.json()
+      console.log(data)
+    }
+  }, 300)
 
   return (
     <div className="form">
       <div className="search-box">
-        <TextField id="fullWidth" fullWidth label="Search a Word" variant="filled" />
+        <TextField
+          label="Search a Word"
+          variant="filled"
+          onChange={event => fetchSearchData(event.target.value)}
+          fullWidth
+        />
       </div>
 
-      <div className="language-select-box">
+      <div className="select-language-box">
         <TextField
           label="Language"
-          value="English"
+          value={selectedLanguage}
           variant="filled"
+          onChange={event => setSelectedLanguage(event.target.value)}
           fullWidth
           select
         >
           {languages.map(language => (
-            <option key={language} value={language}>
+            <MenuItem key={language} value={language}>
               {language}
-            </option>
+            </MenuItem>
           ))}
         </TextField>
       </div>

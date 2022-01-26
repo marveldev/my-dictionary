26e8105dd498bd080ee769a5e979e1272c9bf25c
marveldev <marvellousalika48@gmail.com>
2022-01-26 13:57:52 +0100

    install react fetch hook create function to fetch word definitions

--- src/components/form/Form.jsx
+++ src/components/form/Form.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react'
+import useFetch from 'react-fetch-hook'
 import debounce from 'lodash.debounce'
 import wordList from 'word-list-json'
 import { TextField, ThemeProvider, Autocomplete, Button } from '@mui/material'
@@ -6,41 +7,26 @@ import SearchIcon from '@mui/icons-material/Search'
 
 const Form = ({ setDefinitions, themePalette, setAppMode }) => {
   const [filteredWords, setFilteredWords] = useState([])
-
-  // const changeLanguage = value => {
-  //   setDefinitions([])
-  //   setSelectedLanguage(value)
-  //   setWord('')
-  // }
-
-  // const fetchSearchData = async event => {
-  //   event.preventDefault()
-  //   if (word.trim().length >= 1) {
-  //     const url = `https://api.dictionaryapi.dev/api/v2/entries/${languageShortCode}/${word}`
-  //     try {
-  //       const response = await fetch(url)
-  //       const data = await response.json()
-  //       setAppMode('online')
-  //       setDefinitions(data[0])
-  //     } catch (error) {
-  //       setAppMode('offline')
-  //     }
-  //   } else setDefinitions([])
-  // }
+  const [word, setWord] = useState('')
+  const url = `https://api.dictionaryapi.dev/api/v2/entries/en/${word}`
+  const { isLoading, data, error } = useFetch(url, {depends: [word]})
 
   const doWordFilter = debounce(value => {
     if(value.trim().length >= 1) {
       const wordFilter = wordList?.filter(word => word.startsWith(value.toLowerCase())).slice(0, 30)
       setFilteredWords(wordFilter)
+      setWord(value)
     } else {
       setFilteredWords([])
+      setWord('')
     }
   }, 300)
 
-  const doStuff = (event) => {
+  const fetchWordData = event => {
     event.preventDefault()
-    console.log('ok')
-
+    data && setDefinitions(data[0])
+    isLoading && console.log('is loading')
+    error && console.log(error)
   }
 
   return (
@@ -50,13 +36,19 @@ const Form = ({ setDefinitions, themePalette, setAppMode }) => {
           freeSolo
           disableClearable
           options={filteredWords.map(word => word)}
-          onInputChange={event => doWordFilter(event.target.value)}
+          onChange={(event, value) =>
+            setWord(value)
+          }
           renderInput={params =>
-            <form onSubmit={(event) => doStuff(event)} className="form d-flex m-auto">
+            <form
+              className="form d-flex m-auto"
+              onSubmit={event => fetchWordData(event)}
+            >
               <TextField
                 {...params}
                 variant="filled"
                 label="Search a Word"
+                onChange={event => doWordFilter(event.target.value)}
               />
               <Button type="submit" variant="contained" id="searchButton">
                 <SearchIcon />

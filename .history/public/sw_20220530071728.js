23d0e59bcb1722bc8ef69d79b6aa90ea3cd329a4
marveldev <marvellousalika48@gmail.com>
2022-05-30 07:21:58 +0100

    update service worker file

--- .history/public/sw_20220530071728.js
+++ .history/public/sw_20220530071728.js
@@ -4,7 +4,7 @@ this.addEventListener('install', (event) => {
     caches.open(cacheData).then((cache) => {
       cache.addAll([
         '/static/js/main.03b665c4.js',
-        '/static/css/main.b11b3c65.css',
+        '/static/js/bundle.js',
         '/static/media/searchIllustration.347d84d49db9635a90ff.png',
         '/static/media/loader.fd592f42f44445cbb8e7e12a97382c1f.svg',
         '/icon-192x192.png',
